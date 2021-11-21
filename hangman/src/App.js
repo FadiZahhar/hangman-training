15b1201b07ca30a01155373abad826cdac343d72
Fadi Zahhar <79freelancer@gmail.com>
2021-11-21 13:43:23 +0200

    imporving the app by creating external component

---
+++ hangman/src/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+    return (
+    <div>
+        <h1>Welcome to hangman</h1>
+        <p>Do you want to play the game</p>
+    </div>
+        
+    
+);
+}
--- hangman/src/index.js
+++ hangman/src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-function App() {
-    return (
-    <div>
-        <h1>Welcome to hangman</h1>
-        <p>Do you want to play the game</p>
-    </div>
-        
-    
-    );
-}
 
 ReactDOM.render(
     <App />,
