8d9e1dd5daa980429b0eae7b5f5e4d5b3ec45bae
Fadi Zahhar <79freelancer@gmail.com>
2021-11-21 13:34:11 +0200

    adding my branch for the first time

---
+++ hangman/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+    <div>
+        <h1>Welcome to hangman</h1>
+        <p>Do you want to play the game</p>
+    </div>
+        
+    
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
