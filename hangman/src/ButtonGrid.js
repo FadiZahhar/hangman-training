3776ab6fb45aeec587c99ce2e948121cc172979b
Fadi Zahhar <79freelancer@gmail.com>
2021-11-21 17:50:37 +0200

    creating the button of the interface by creating double components button grid and button component

---
+++ hangman/src/ButtonGrid.js
@@ -0,0 +1,19 @@
+import Button from './button';
+
+export default function() {
+    
+    let letters = ['A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','K','R','S','T','U','W','X','Y','Z'];
+
+    let buttons = letters.map((letter,index)=>(
+        <Button
+            value={letter}
+            key={index}
+        />
+    ));
+    return (
+        <div>
+            {buttons}
+        </div>
+    );
+
+}
--- hangman/src/GameBoard.js
+++ hangman/src/GameBoard.js
@@ -1,10 +1,13 @@
 import LetterGrids from "./LetterGrids";
+import ButtonGrid from "./ButtonGrid";
+
 export default function GameBoard({secretWord}) {
     return (
         <div>
             <LetterGrids secretWord={secretWord} 
             guessedLetters={['R','a','t']}
             />
+            <ButtonGrid />
         </div>
     );
 }
