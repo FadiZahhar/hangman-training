b2156dffb6e4999cfa4b189efb14f0e761056a47
Anthony sayegh <Sayeghanthony89012@gmail.com>
2021-11-22 16:28:23 +0200

    testing index.jsx

--- hangman/src/index.jsx
+++ hangman/src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+function App(){
+    return (
+    <div>
+        <h1>Welcome to Hangman!</h1>
+        <p>Do you want to play </p>
+    </div>
+    );
+}
+
 ReactDom.render(
-    <p>Hello, World</p>,
+    <App name="Yara"/>,
     document.getElementById('root')
 );
