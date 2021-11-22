f5e59b52fdf4b61b3db75d1f6fa407ff570f06d2
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-11-22 17:33:44 +0200

    game-board component

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import GameBoard from './game-board';
 
 export default function App() {
     return (
         <div>
             <h1>Welcome to Hangman!</h1>
             <p>do you want to play a game? </p>
+
+            <div>
+                <GameBoard secretWord="React" />
+            </div>
         </div>
     )
 }
---
+++ hangman/src/game-board.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ({ secretWord }) {
+    return (
+        <div>
+            {secretWord}
+        </div>
+    )
+}
