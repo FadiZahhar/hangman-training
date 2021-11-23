e1239d03e2fef91ef4ed9af8555949ba980169ea
Anthony sayegh <Sayeghanthony89012@gmail.com>
2021-11-23 11:47:32 +0200

    function erasing letter when clicked

--- hangman/src/button.js
+++ hangman/src/button.js
@@ -1,6 +1,23 @@
-export default function ({value}){
+import {useState} from 'react';
 
+export default function ({value}){
+    const [isClicked, setIsClicked] = useState(false);
+    let className='button';
+    if(isClicked){
+        className+=' guessed';
+    }
+    function clickHandler(){
+        setIsClicked(true);
+    }
+    
+    console.log('button');
     return(
-        <button className="button">{value}</button>
+        <button 
+            className={className}
+            onClick={clickHandler}
+            
+        >
+            {value}
+        </button>
     );
 }
