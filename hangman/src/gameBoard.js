9aff605e26a9a18ccfb485762b036e470879238a
MarieThereseMelhem <marietherese.melhem@hotmail.com>
2021-11-30 01:20:08 +0200

    adding button grid

--- hangman/src/gameBoard.js
+++ hangman/src/gameBoard.js
@@ -1,11 +1,12 @@
 import LetterGrid from './letterGrid';
-
+import ButtonGrid from './buttonGrid';
 
 export default function({secretWord}){
     return(
         <div>
             <LetterGrid secretWord={secretWord}
             guessedLetters={['a','r','t']} />
+            <ButtonGrid />
         </div>
     )
 }
